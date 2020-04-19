4f4689cd36efbef7aeddaa298c9249fd66d7ef6f
Gokul <gokul.uf@gmail.com>
2020-04-19 23:06:54 +0200

    added for producing gradient background

--- src/ray.py
+++ src/ray.py
@@ -1,4 +1,5 @@
 from vec3 import Vec3
+from vec3 import unit_vector
 
 class Ray:
     def __init__(self, origin = None, direction = None):
@@ -7,3 +8,23 @@ class Ray:
 
     def at(self, t):
         return self.origin + t * self.direction
+
+def ray_color(r, start_color = None, end_color = None):
+    # This implements the linear interpolation discussed in the notes.
+    # change the start and end vectors for different gradients
+
+    if not isinstance(r, Ray):
+        raise TypeError("Expected 'Ray' object, got {}".format(type(r)))
+    unit_direction = unit_vector(r.direction)
+
+    if not start_color:
+        start_color = Vec3(1, 1, 1) # White
+
+    if not end_color:
+        end_color = Vec3(0.5, 0.7, 1.0) # Blue
+
+    # scale [-1, 1] to [0, 1]
+    t = 0.5*(unit_direction.y + 1.0)
+
+    # do the linear interpolation from start to end color
+    return (1. - t)*start_color + t*end_color
