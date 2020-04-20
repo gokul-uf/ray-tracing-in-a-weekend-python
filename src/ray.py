b0c61af5588fe89fb4543e683ef41b4fc581de3d
Gokul <gokul.uf@gmail.com>
2020-04-20 02:12:52 +0200

    made checking for None default values in ray_color more pythonic, added ray_color_sphere for the display with sphere in it

--- src/ray.py
+++ src/ray.py
@@ -1,6 +1,8 @@
 from vec3 import Vec3
 from vec3 import unit_vector
 
+from sphere_utils import hit_sphere
+
 class Ray:
     def __init__(self, origin = None, direction = None):
         self.origin = origin if origin is not None else Vec3()
@@ -15,13 +17,38 @@ def ray_color(r, start_color = None, end_color = None):
 
     if not isinstance(r, Ray):
         raise TypeError("Expected 'Ray' object, got {}".format(type(r)))
+
+    start_color = start_color if start_color is not None else Vec3(1, 1, 1) # White
+    end_color = end_color if end_color is not None else Vec3(0.5, 0.7, 1.) # Blue
+
     unit_direction = unit_vector(r.direction)
 
-    if not start_color:
-        start_color = Vec3(1, 1, 1) # White
+    # scale [-1, 1] to [0, 1]
+    t = 0.5*(unit_direction.y + 1.0)
 
-    if not end_color:
-        end_color = Vec3(0.5, 0.7, 1.0) # Blue
+    # do the linear interpolation from start to end color
+    return (1. - t)*start_color + t*end_color
+
+def ray_color_sphere(r, start_color = None, end_color = None, 
+        sphere_center = None, radius = 0.5, hit_color=None):
+    # In addition to the gradient as done in ray_color, this also checks if 
+    # the ray hits a sphere at center `c` and radius `radius` or not.
+    # If it does, output `hit_color`. The defaults for these are as per the notes
+    # in notes/4-sphere.md
+
+    if not isinstance(r, Ray):
+        raise TypeError("Expected 'Ray' object, got {}".format(type(r)))
+
+    sphere_center = sphere_center if sphere_center is not None else Vec3(0, 0, -1)
+    hit_color = hit_color if hit_color is not None else Vec3(1, 0, 0) # Red
+
+    if hit_sphere(sphere_center, radius, r): # It's a hit!
+        return hit_color
+
+    unit_direction = unit_vector(r.direction)
+
+    start_color = start_color if start_color is not None else Vec3(1, 1, 1) # White
+    end_color = end_color if end_color is not None else Vec3(0.5, 0.7, 1.) # Blue
 
     # scale [-1, 1] to [0, 1]
     t = 0.5*(unit_direction.y + 1.0)
